fa1a8d23c58ef38c0658958a8aed636127408ee7
mike <michaelmensah56211@gmail.com>
2022-10-09 22:56:10 +0000

    kotlin practice for android apps

---
+++ app/src/main/java/com/example/myfirstapplication/SecondFragment.kt
@@ -0,0 +1,62 @@
+package com.example.myfirstapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.myfirstapplication.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    val args: SecondFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val count=args.myArg
+        val countText = getString(R.string.random_heading,count)
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        //code to get a random number between 0 and the count.
+        val random = java.util.Random()
+        var randomNumber=0
+        if (count>0){
+            randomNumber=random.nextInt(count + 1)
+        }
+
+        //code to convert that number into a string and set it as the text for textview_random.
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
